79f821e0a16a393d23d2560254e8ef674fed16c5
Shane Gordon <se2gordon@students.latrobe.edu.au>
2018-04-01 19:49:34 +1000

    Added function pdb-dl to download pdb from protein databank using alphanumeric code.

--- aliases.plugin.zsh
+++ aliases.plugin.zsh
@@ -11,11 +11,22 @@ get_file_name_without_ext() {
   echo "$__fname"
 }
 
+dl_pdb_structure() {
+  # takes single argument of four-character pdb code
+  # todo: add check for validity of code
+  local __pdb_code="$1"
+  printf "Downloading PDB code %s to %s.pdb\n" "$__pdb_code" "$__pdb_code"
+  wget --quiet "http://www.pdb.org/pdb/files/$__pdb_code.pdb" -O "$__pdb_code.pdb"
+}
+
 # }}}1
 
 # aliases # {{{1
 
 # return filename without path or extension
 alias get_fname='get_file_name_without_ext'
+
+# download structure from pdb using four character alphanumeric code
+alias pdb-dl="dl_pdb_structure"
 
 # }}}1
